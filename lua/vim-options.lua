d161c976e03ddc957c36f2d443997d20e2467f5f
superRFstrat <superscript90@gmail.com>
2024-02-05 21:14:45 -0600

    [A] All my current shit

---
+++ lua/vim-options.lua
@@ -0,0 +1,9 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+vim.wo.number = true
+vim.wo.relativenumber = true
